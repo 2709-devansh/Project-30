18f586268b6f083cd2c8d5877c565c8a98b3b93e
Devansh Panthi <69043897+2709-devansh@users.noreply.github.com>
2020-09-07 15:30:04 +0530

    Update box.js

--- box.js
+++ box.js
@@ -12,7 +12,7 @@ class box{
         World.add(world, this.body);
     }
     display(){
-        if(this.body.speed<3){
+        if(this.body.speed<5){
            var angle = this.body.angle;
            push();
            translate(this.body.position.x,this.body.position.y);
